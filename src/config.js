35646010e3c4dd122144599a991d97985c59e56f
Javier Soria <javier.soria@gmail.com>
2019-10-28 14:33:45 -0700

    - Add endpoint to env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  //  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://agile-coast-28485.herokuapp.com/api',
+   API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
    TOKEN_KEY: 'activity-client-auth-token',
 }
