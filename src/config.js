d6621ca6a0d840ef2805cf5b69227463a7bf66ac
Javier Soria <javier.soria@gmail.com>
2019-10-28 14:31:25 -0700

    - Update config with deploy API endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-   API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  //  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://agile-coast-28485.herokuapp.com/api',
    TOKEN_KEY: 'activity-client-auth-token',
- }
+}
