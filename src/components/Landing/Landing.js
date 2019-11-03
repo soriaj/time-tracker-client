2dcbdef23610f945f63bb334d117b09ad136979c
Javier Soria <javier.soria@gmail.com>
2019-11-02 21:17:15 -0700

    - Style see demo button on landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,10 +9,15 @@ class Landing extends Component {
          <div className="registration">
             <h1>Welcome to Time Tracker</h1>
             <p>An application for those that want to keep track of their customer activities organized and readily availabe for review</p>
-            <div>
+            <div className='btn-box'>
                <button className="welcome-btn">
                   <NavLink to='/register' className="sign-up-link">
                      Sign Up
+                  </NavLink>
+               </button>
+               <button className="welcome-demo">
+                  <NavLink to='/demo' className="sign-up-link">
+                     See Demo
                   </NavLink>
                </button>
             </div>
